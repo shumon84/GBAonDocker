10f27e5e757e087f3c1579a6bf63b16460eec2a4
shumon84 <cameremon84@gmail.com>
2018-07-04 09:16:30 +0900

    gradation.pngを差し替え

--- chap04/sec03/gradation/include/gradation.h
+++ chap04/sec03/gradation/include/gradation.h
@@ -7,7 +7,7 @@
 //	+ bitmap not compressed
 //	Total size: 76800 = 76800
 //
-//	Time-stamp: 2018-06-29, 22:55:12
+//	Time-stamp: 2018-07-04, 09:10:39
 //	Exported by Cearn's GBA Image Transmogrifier, v0.8.13
 //	( http://www.coranac.com/projects/#grit )
 //
